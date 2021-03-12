6e587dd31c34696b7833e1764db1c97d07cfb412
Juliancio Carvalho <juliancio_alves@hotmail.com>
2021-03-12 11:18:09 -0300

    feat: statement by date

--- src/index.js
+++ src/index.js
@@ -56,6 +56,20 @@ app.get("/statement", verifyIfExixtsAccountCPF, (request, response) => {
   return response.json(customer.statement);
 });
 
+app.get("/statement/date", verifyIfExixtsAccountCPF, (request, response) => {
+  const { customer } = request;
+  const { date } = request.query;
+  const dateFormat = new Date(date + " 00:00");
+
+  const statement = customer.statement.filter(
+    (statement) =>
+      statement.created_at.toDateString() ===
+      new Date(dateFormat).toDateString()
+  );
+
+  return response.json(statement);
+});
+
 app.post("/deposit", verifyIfExixtsAccountCPF, (request, response) => {
   const { description, amount } = request.body;
   const { customer } = request;
