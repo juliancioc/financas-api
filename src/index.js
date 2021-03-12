47c25925e46336b2be094b1098118b5411dd5207
Juliancio Carvalho <juliancio_alves@hotmail.com>
2021-03-11 21:07:58 -0300

    feat: withdraw

--- src/index.js
+++ src/index.js
@@ -19,6 +19,18 @@ function verifyIfExixtsAccountCPF(request, response, next) {
   return next();
 }
 
+function getBalance(statement) {
+  const balance = statement.reduce((acc, operation) => {
+    if (operation.type === "credit") {
+      return acc + operation.amount;
+    } else {
+      return acc - operation.amount;
+    }
+  }, 0);
+
+  return balance;
+}
+
 app.post("/account", (request, response) => {
   const { cpf, name } = request.body;
   const customerAlreadyExists = customers.some(
@@ -56,6 +68,26 @@ app.post("/deposit", verifyIfExixtsAccountCPF, (request, response) => {
   };
 
   customer.statement.push(statementOperations);
+
+  return response.status(201).send();
+});
+
+app.post("/withdraw", verifyIfExixtsAccountCPF, (request, response) => {
+  const { amount } = request.body;
+  const { customer } = request;
+  const balance = getBalance(customer.statement);
+
+  if (balance < amount) {
+    return response.status(400).json({ error: "Insufficient funds!" });
+  }
+
+  const statementOperation = {
+    amount,
+    created_at: new Date(),
+    type: "debit",
+  };
+
+  customer.statement.push(statementOperation);
 
   return response.status(201).send();
 });
